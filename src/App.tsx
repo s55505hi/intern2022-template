51fd8e2047c3b90318e1ab61eddaeb0afad83a0b
HP071\伊藤晴仁 <s55505hi@fukui.kosen-ac.jp>
2022-08-19 09:57:53 +0900

    calendar app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Component, useState } from "react";
 import {
   Center,
   HStack,
@@ -9,78 +9,203 @@ import {
   Link,
   Text,
 } from "@chakra-ui/react";
-import type { ImageProps } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import reactLogo from "./assets/react.svg";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Repeat } from "typescript-tuple";
+import { Square } from "@chakra-ui/react";
+import dayjs from "dayjs";
+import ja from "dayjs/locale/ja";
+import { useTable } from "react-table";
+import { columns } from "./tableData";
 
-function App() {
-  const [count, setCount] = useState(0);
+dayjs.locale(ja);
 
-  const MotionImage = motion<Omit<ImageProps, "transition">>(Image);
-  return (
-    <Center
-      sx={{
-        width: "100vw",
-        height: "100vh",
-        margin: "0 auto",
-        padding: "2rem",
-        textAlign: "center",
-      }}
-    >
-      <Box>
-        <HStack justify="center">
-          <Link
-            as="a"
-            href="https://vitejs.dev"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Image
-              src="/vite.svg"
-              p={6}
-              sx={{ willChange: "filter", height: "6em" }}
-              _hover={{ filter: "drop-shadow(0 0 2em #646cffaa)" }}
-              alt="Vite logo"
-              m={0}
-            />
-          </Link>
-          <Link
-            as="a"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <MotionImage
-              as={motion.img}
-              animate={{ transform: ["rotate(0deg)", "rotate(360deg)"] }}
-              transition={{
-                repeat: Infinity,
-                duration: 20,
-                ease: "linear",
-              }}
-              src={reactLogo}
-              p={6}
-              sx={{ willChange: "filter", height: "6em" }}
-              _hover={{ filter: "drop-shadow(0 0 2em #646cffaa)" }}
-              alt="React logo"
-            />
-          </Link>
-        </HStack>
-        <Heading as="h1">Vite + React</Heading>
-        <Box p={8}>
-          <Button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </Button>
-          <Text>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </Text>
-        </Box>
-        <Text sx={{ color: "#888" }}>
-          Click on the Vite and React logos to learn more
-        </Text>
-      </Box>
-    </Center>
-  );
+const data = null;
+
+//dayjs初期化
+const now = dayjs();
+
+//曜日列挙
+enum Days {
+  Sun,
+  Mon,
+  Tue,
+  Wed,
+  Thu,
+  Fri,
+  Sat,
 }
 
-export default App;
+//曜日文字列配列
+const week = Object.keys(Days).filter((v) => isNaN(Number(v)));
+// console.log(week[week.length - 1]);
+
+//月列挙
+enum Month {
+  Jan,
+  Feb,
+  Mar,
+  Apr,
+  May,
+  Jun,
+  Jul,
+  Aug,
+  Sept,
+  Oct,
+  Nov,
+  Dec,
+}
+
+//予定
+type Schedule = {
+  title: string;
+  day: string;
+  startTime: string;
+  endTime: string;
+  description: string;
+};
+
+//日ごとの要素
+type DateProps = {
+  year: number | dayjs.Dayjs;
+  month: Month | dayjs.Dayjs;
+  day: Days | dayjs.Dayjs;
+  dayNumber: number | dayjs.Dayjs;
+  schedule?: Schedule[];
+  onClick: () => void;
+};
+
+//日ごとの内容表示
+const Date = (props: DateProps) => (
+  <div className="date-container">
+    <div>{props.dayNumber.valueOf()}</div>
+    {props.schedule ? (
+      <div className="date-schedule">${props.schedule[0].title}</div>
+    ) : (
+      ""
+    )}
+  </div>
+);
+
+//1月ごとの要素
+type MonthState = Repeat<DateProps, 42>;
+
+type MonthProps = {
+  squares: MonthState;
+  onClick: (i: number) => void;
+};
+
+const CalendarBoard = (props: MonthProps) => {
+  const renderDate = (i: number) => (
+    <Date {...props.squares[i]} onClick={() => props.onClick(i)} />
+  );
+  const elm = (
+    <tr>
+      {(() => {
+        const items = [];
+        items.push(<tr></tr>);
+        for (let i = 0; i < props.squares.length; i++) {
+          for (let j = Days.Sun; j < Days.Sat + 1; j++) {
+            items.push(<td>{renderDate(j)}</td>);
+          }
+        }
+        console.log(items);
+        return items;
+      })()}
+    </tr>
+  );
+
+  return elm;
+};
+
+type Data = {
+  squares: MonthState;
+};
+
+type CalendarState = {
+  readonly days: Data[];
+  readonly index: number;
+};
+
+const Calendar = () => {
+  const nowData = getCalendarData(0);
+  const calendarTable: DateProps[] = [];
+  // const thisMonth = (props: MonthProps) => {
+  //   squares: [null];
+  // };
+
+  for (let i = 0; i < 42; i++) {
+    const thisMonth = {
+      year: nowData[0],
+      month: nowData[0],
+      day: nowData[1],
+      dayNumber: i + 1,
+      onClick: () => handleClick(i),
+    };
+    calendarTable.push(thisMonth);
+  }
+
+  // const current = state.days[state.index];
+
+  // const next: Date = (({ year, month, day, dayNumber }) => {
+  //   return {
+  //     dayNumber: dayNumber + 1,
+  //     month:
+  //       dayNumber > 31
+  //         ? month + 1 > Month.Dec
+  //           ? Month.Jan
+  //           : month + 1
+  //         : month,
+  //     year: year,
+  //     day: day + 1 > Days.Sat ? Days.Sun : day + 1,
+  //     schedule: [],
+  //   };
+  // })(current);
+
+  // setState(({ days, index }) => {
+  //   const newDate = days.slice(0, index + 1).concat(next);
+
+  //   return {
+  //     days: newDate,
+  //     index: newDate.length - 1,
+  //   };
+  // });
+  const handleClick = (i: number) => {
+    return;
+  };
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr className="dayHeader">
+            {week.map((day, index) => {
+              return <th key={index}>{day}</th>;
+            })}
+          </tr>
+
+          {/* <tr>
+            {state.days.map((days, index) => {
+              return <td key={index}>{days.dayNumber}</td>;
+            })}
+          </tr> */}
+          <CalendarBoard squares={calendarTable} onClick={handleClick} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const getCalendarData = (i: number) => {
+  const month = now.add(i, "month");
+  const startDate = month.startOf("month");
+  const endDate = month.endOf("month");
+  const startDay = startDate.get("day");
+  const endDay = endDate.get("day");
+  return [month, startDay, endDay, startDate, endDate];
+};
+
+export default Calendar;
---
+++ src/tableData.tsx
@@ -0,0 +1,9 @@
+export const columns = [
+  { Header: "日", accessor: "Sun" },
+  { Header: "月", accessor: "Mon" },
+  { Header: "火", accessor: "Tue" },
+  { Header: "水", accessor: "Wed" },
+  { Header: "木", accessor: "Thu" },
+  { Header: "金", accessor: "Fri" },
+  { Header: "土", accessor: "Sat" },
+];
